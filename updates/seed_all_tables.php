aa55d15261d9b7be69f272ba62c75d72d592aa50
Jérémy Gaulin <jeremy@bnb.re>
2016-08-02 17:28:42 +0200

    New french states

--- updates/seed_all_tables.php
+++ updates/seed_all_tables.php
@@ -2,7 +2,6 @@
 
 use October\Rain\Database\Updates\Seeder;
 use RainLab\Location\Models\Country;
-use RainLab\Location\Models\State;
 
 class SeedAllTables extends Seeder
 {
@@ -590,33 +589,24 @@ class SeedAllTables extends Seeder
 
         $fr = Country::whereCode('FR')->first();
         $fr->states()->createMany([
-            ['code' => 'A', 'name' => 'Alsace'],
-            ['code' => 'B', 'name' => 'Aquitaine'],
-            ['code' => 'C', 'name' => 'Auvergne'],
-            ['code' => 'D', 'name' => 'Basse-Normandie'],
-            ['code' => 'E', 'name' => 'Bourgogne'],
-            ['code' => 'F', 'name' => 'Bretagne'],
-            ['code' => 'G', 'name' => 'Centre – Val de Loire'],
-            ['code' => 'H', 'name' => 'Champagne-Ardenne'],
-            ['code' => 'I', 'name' => 'Corse'],
-            ['code' => 'J', 'name' => 'Franche-Comté'],
-            ['code' => 'K', 'name' => 'Haute-Normandie'],
-            ['code' => 'L', 'name' => 'Île-de-France'],
-            ['code' => 'M', 'name' => 'Languedoc-Roussillon'],
-            ['code' => 'N', 'name' => 'Limousin'],
-            ['code' => 'O', 'name' => 'Lorraine'],
-            ['code' => 'P', 'name' => 'Midi-Pyrénées'],
-            ['code' => 'Q', 'name' => 'Nord-Pas-de-Calais'],
-            ['code' => 'R', 'name' => 'Pays de la Loire'],
-            ['code' => 'S', 'name' => 'Picardie'],
-            ['code' => 'T', 'name' => 'Poitou-Charentes'],
-            ['code' => 'U', 'name' => 'Provence-Alpes-Côte d\'Azur'],
-            ['code' => 'V', 'name' => 'Rhône-Alpes'],
-            ['code' => 'GP', 'name' => 'Guadeloupe'],
-            ['code' => 'GF', 'name' => 'Guyane (française)'],
-            ['code' => 'MQ', 'name' => 'Martinique'],
-            ['code' => 'RE', 'name' => 'Réunion'],
-            ['code' => 'YT', 'name' => 'Mayotte']
+            ['code' => 'ARA',  'name' => 'Auvergne-Rhône-Alpes'],
+            ['code' => 'BFC',  'name' => 'Bourgogne-Franche-Comté'],
+            ['code' => 'BZH',  'name' => 'Bretagne'],
+            ['code' => 'CVL',  'name' => 'Centre–Val-de-Loire'],
+            ['code' => 'COR',  'name' => 'Corse'],
+            ['code' => 'GP',   'name' => 'Guadeloupe'],
+            ['code' => 'GF',   'name' => 'Guyane'],
+            ['code' => 'GE',   'name' => 'Grand-Est'],
+            ['code' => 'HF',   'name' => 'Hauts-de-France'],
+            ['code' => 'IDF',  'name' => 'Île-de-France'],
+            ['code' => 'MQ',   'name' => 'Martinique'],
+            ['code' => 'YT',   'name' => 'Mayotte'],
+            ['code' => 'NOR',  'name' => 'Normandie'],
+            ['code' => 'PL',   'name' => 'Pays-de-la-Loire'],
+            ['code' => 'NA',   'name' => 'Nouvelle-Aquitaine'],
+            ['code' => 'OCC',  'name' => 'Occitanie'],
+            ['code' => 'PACA', 'name' => 'Provence-Alpes-Côte-d\'Azur'],
+            ['code' => 'RE',   'name' => 'Réunion'],
         ]);
 
         $nz = Country::whereCode('NZ')->first();
