b3ba2d5f90efb1247c5be68fdcf9fe3e79a28751
Samuel Georges <sam@daftspunk.com>
2016-08-31 07:41:41 +1000

    Tidy up code

--- updates/add_country_pinned_flag.php
+++ updates/add_country_pinned_flag.php
@@ -1,6 +1,7 @@
 <?php namespace RainLab\Location\Updates;
 
 use Schema;
+use RainLab\Location\Models\Country;
 use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
@@ -11,6 +12,8 @@ class AddCountryPinnedFlag extends Migration
         Schema::table('rainlab_location_countries', function(Blueprint $table) {
             $table->boolean('is_pinned')->default(false);
         });
+
+        Country::whereIn('code', ['AU', 'CA', 'GB', 'US'])->update(['is_pinned' => 1]);
     }
 
     public function down()
