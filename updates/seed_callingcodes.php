19f5ded4e85c6a3d8136937e6d7fd2b979524de8
Samuell <Samuell1@users.noreply.github.com>
2020-08-14 23:16:18 +0200

    Add calling codes

---
+++ updates/seed_callingcodes.php
@@ -0,0 +1,12 @@
+<?php namespace RainLab\Location\Updates;
+
+use October\Rain\Database\Updates\Seeder;
+use RainLab\Location\Models\Country;
+
+class SeedCallingCodes extends Seeder
+{
+    public function run()
+    {
+            
+    }
+}
