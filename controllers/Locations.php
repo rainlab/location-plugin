938ae74b27a677283d1dc2e7676345f0565002e2
Luke Towers <github@luketowers.ca>
2020-11-24 16:04:58 -0600

    Update controllers/Locations.php

--- controllers/Locations.php
+++ controllers/Locations.php
@@ -47,9 +47,8 @@ class Locations extends Controller
     public function relationExtendViewWidget($widget)
     {
         $widget->bindEvent('list.injectRowClass', function ($record) {
-            
-            if(!$record->is_enabled) {
-                return  'safe disabled';
+            if (!$record->is_enabled) {
+                return 'safe disabled';
             }
         });
     }
